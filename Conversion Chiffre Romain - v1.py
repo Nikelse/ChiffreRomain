7e4bab33dd734067f76aab781a09ca675d82b308
Nikelse <felnik@outlook.fr>
2020-05-20 12:07:41 +0200

    add source

---
+++ Conversion Chiffre Romain - v1.py
@@ -0,0 +1,59 @@
+﻿# conversion nombre entier en chiffre romain
+# version procedurale (v1.0)
+# NF.NSI
+
+limite = 5000
+
+saisie = int(input('Entrez un entier compris entre 1 et '+str(limite)))
+while not(saisie >= 1 and saisie <= limite) :
+    saisie = int(input('Entrez un entier compris entre 1 et '+str(limite)))
+
+# chaine resultante de concatenation pour la constitution du chiffre romain
+s = ""
+# nombre entier à convertir pour la décrementation
+n = saisie
+
+while n >= 1000:
+    s += "M"
+    n -= 1000
+if n >= 900:
+    s += "CM"
+    n -= 900
+if n >= 500:
+    s += "D"
+    n -= 500
+if n >= 400:
+    s += "CD"
+    n -= 400
+
+while n >= 100:
+    s += "C"
+    n -= 100
+if n >= 90:
+    s += "XC"
+    n -= 90
+if n >= 50:
+    s += "L"
+    n -= 50
+if n >= 40:
+    s += "XL"
+    n -= 40
+
+while n >= 10:
+    s += "X"
+    n -= 10
+if n >= 9:
+    s += "IX"
+    n -= 9
+if n >= 5:
+    s += "V"
+    n -= 5
+if n >= 4:
+    s += "IV"
+    n -= 4
+
+while n >= 1:
+    s += "I"
+    n -= 1
+
+print("Le nombre entier",saisie, "vaut",s,"en chiffre romain")
---
+++ Conversion Chiffre Romain - v2.py
@@ -0,0 +1,39 @@
+﻿# conversion nombre entier en chiffre romain
+# version tableau (v2.0)
+# NF.NSI
+
+# tableau de conversion
+chiffres = {
+    1    : "I", 2   : "II", 3   : "III", 4   : "IV", 5   : "V", 6   : "VI", 7   : "VII", 8   : "VIII", 9    : "IX",
+    10   : "X", 20  : "XX", 30  : "XXX", 40  : "XL", 50  : "L", 60  : "LX", 70  : "LXX", 80  : "LXXX", 90   : "XC",
+    100  : "C", 200 : "CC", 300 : "CCC", 400 : "CD", 500 : "D", 600 : "DC", 700 : "DCC", 800 : "DCCC", 900  : "CM",
+    1000 : "M"
+}
+
+# limite de saisie
+limite = 5000
+
+# debut du programme
+# ------------------------------------------------------------------------
+saisie = int(input('Entrez un entier compris entre 1 et '+str(limite)))
+while not(saisie >= 1 and saisie <= limite) :
+    saisie = int(input('Entrez un entier compris entre 1 et '+str(limite)))
+
+# tri des clefs du dictionnaire en ordre décroissant sur l'index
+chiffres = sorted(chiffres.items(), key=lambda t: t[0], reverse=True)
+
+s = ""
+n = saisie
+
+while (n > 0) :
+    for nombre, romain in chiffres :
+        # N est plus grand que la position de la clef dans le tableau
+        if n >= nombre :
+            # decrementation du nombre
+            n -= nombre
+            # concatenation du chiffre romain
+            s += romain
+            # reset du compteur dans la boucle
+            break
+
+print("Le nombre entier",saisie, "vaut",s,"en chiffre romain (v2)")
